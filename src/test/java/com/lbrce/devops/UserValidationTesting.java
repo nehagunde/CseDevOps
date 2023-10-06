f709d3c75bc350ccdb7f552364e32454c8384a09
nehagunde <94432480+nehagunde@users.noreply.github.com>
2023-10-06 15:13:14 +0530

    Add files via upload

---
+++ src/test/java/com/lbrce/devops/UserValidationTesting.java
@@ -0,0 +1,27 @@
+package com.lbrce.devops;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UserValidationTesting {
+ @Test
+ public void testCase1()
+ {
+	 UserValidation uv=new UserValidation();
+	 Assert.assertEquals(false,uv.check("srinu", "srinu@12345"));
+ }
+ @Test
+ public void testCase2()
+ {
+	 UserValidation uv=new UserValidation();
+	 Assert.assertEquals(false,uv.check("Srinu", "Srinu@12345"));
+ }
+ @Test
+ public void testCase3()
+ {
+	 UserValidation uv=new UserValidation();
+	 Assert.assertEquals(true,uv.check("lbrce", "lbrce@76"));
+ }
+
+
+}
